618a8a8266716ba223cf8c4f7ca933d5b3c889b4
chanlam1102 <51082247+chanlam1102@users.noreply.github.com>
2020-08-24 21:24:07 +0700

    Add files via upload

---
+++ Entity/Question.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entity
+{
+    public class Question
+    {
+        public int id { get; set; }
+        public string content { get; set; }
+        public float score { get; set; }
+        public int duration { get; set; }
+        public List<Answer> answers = new List<Answer>();
+
+        public bool multipleChoice
+        {
+            get
+            {
+                int count = 0;
+                foreach(Answer a in answers)
+                {
+                    if(a.corrected)
+                    {
+                        count++;
+                    }
+                }
+                return count > 1;
+            }
+        }
+
+    }
+
+    public class Answer
+    {
+        public int id { get; set; }
+        public string content { get; set; }
+        public bool corrected { get; set; }
+    }
+}
